1e5d147d4544667c3a7109730b0683057dc32cec
PetrRyb <petr.rybin@infis.cz>
2025-04-11 18:50:38 +0200

    Add files via upload

---
+++ random choice.py
@@ -0,0 +1,92 @@
+import random
+
+věci = ["kabát", "muffin", "chleba", "kaiserka", "mléko", "donut", "rajče", "řízek", "kuřecí nugety", "herní počítač"]
+
+nahodna_vec = random.choice(věci)    
+nahodna_vec2 = random.choice(věci)
+nahodna_vec3 = random.choice(věci)
+nahodna_vec4 = random.choice(věci)
+nahodna_vec5 = random.choice(věci)
+nahodna_vec6 = random.choice(věci)
+nahodna_vec7 = random.choice(věci)
+nahodna_vec8 = random.choice(věci)
+nahodna_vec9 = random.choice(věci)
+nahodna_vec10 = random.choice(věci)
+nahodna_vec11 = random.choice(věci)
+nahodna_vec12 = random.choice(věci)
+nahodna_vec13 = random.choice(věci)
+nahodna_vec14 = random.choice(věci)
+nahodna_vec15 = random.choice(věci)
+nahodna_vec16 = random.choice(věci)
+nahodna_vec17 = random.choice(věci)
+nahodna_vec18 = random.choice(věci)
+nahodna_vec19 = random.choice(věci)
+nahodna_vec20 = random.choice(věci)
+nahodna_vec21= random.choice(věci)
+nahodna_vec22= random.choice(věci)
+nahodna_vec23= random.choice(věci)
+nahodna_vec24 = random.choice(věci)
+nahodna_vec25 = random.choice(věci)
+nahodna_vec26 = random.choice(věci)
+nahodna_vec27 = random.choice(věci)
+nahodna_vec28 = random.choice(věci)
+nahodna_vec29 = random.choice(věci)
+
+if nahodna_vec == nahodna_vec2: 
+    print(f"měli bychom si koupit tuto jinou položku {nahodna_vec20}")
+else:
+    print(f"měli bychom si koupit {nahodna_vec}.")
+    print(f"ne, měli bychom si koupit {nahodna_vec2}.")
+if nahodna_vec3== nahodna_vec4:
+    print(f"měli bychom si koupit tuto jinou položku {nahodna_vec21}")
+else:
+    print (f"měli bychom si koupit {nahodna_vec3}")
+    print(f"ne, měli bychom si tuto jinou položku {nahodna_vec4}")
+    
+if nahodna_vec5 == nahodna_vec6: 
+    print(f"měli bychom si koupit tuto jinou položku {nahodna_vec22}")
+else:
+    print(f"měli bychom si koupit {nahodna_vec5}.")
+    print(f"ne, měli bychom si koupit {nahodna_vec6}.")
+
+if nahodna_vec7 == nahodna_vec8: 
+    print(f"měli bychom si koupit tuto jinou položku {nahodna_vec23}")
+else:
+    print(f"měli bychom si koupit {nahodna_vec7}.")
+    print(f"ne, měli bychom si koupit {nahodna_vec8}.")
+
+if nahodna_vec9 == nahodna_vec10: 
+    print(f"měli bychom si koupit tuto jinou položku {nahodna_vec24}")
+else:
+    print(f"měli bychom si koupit {nahodna_vec9}.")
+    print(f"ne, měli bychom si koupit {nahodna_vec10}.")
+
+if nahodna_vec11 == nahodna_vec12: 
+    print(f"měli bychom si koupit tuto jinou položku {nahodna_vec25}")
+else:
+    print(f"měli bychom si koupit {nahodna_vec11}.")
+    print(f"ne, měli bychom si koupit {nahodna_vec12}.")
+
+if nahodna_vec13 == nahodna_vec14: 
+    print(f"měli bychom si koupit tuto jinou položku {nahodna_vec26}")
+else:
+    print(f"měli bychom si koupit {nahodna_vec13}.")
+    print(f"ne, měli bychom si koupit {nahodna_vec14}.")
+
+if nahodna_vec15 == nahodna_vec16:
+    print(f"měli bychom si koupit tuto jinou položku {nahodna_vec27}")
+else:
+    print(f"měli bychom si koupit {nahodna_vec15}.")
+    print(f"ne, měli bychom si koupit {nahodna_vec16}.")
+
+if nahodna_vec17 == nahodna_vec18:
+    print(f"měli bychom si koupit tuto jinou položku {nahodna_vec28}")
+else:
+    print(f"měli bychom si koupit {nahodna_vec17}.")
+    print(f"ne, měli bychom si koupit {nahodna_vec18}.")
+
+if nahodna_vec19 == nahodna_vec20: 
+    print(f"měli bychom si koupit tuto jinou položku {nahodna_vec29}")
+else:
+    print(f"měli bychom si koupit {nahodna_vec19}.")
+    print(f"ne, měli bychom si koupit {nahodna_vec20}.")
